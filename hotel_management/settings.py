6d4af86f42912756732661b7b56527097b61c111
sumeet32112 <85902775+sumeet32112@users.noreply.github.com>
2022-01-29 17:39:03 +0530

    Update settings.py

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +129,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     STATIC_DIR,
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT=STATIC_DIR
 MEDIA_URL='/images/'
 
